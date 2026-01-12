e35e6f96c6c19fe9bc82b46a80ef162a3e89437a
Benas <benas.rag@gmail.com>
2026-01-12 12:16:07 +0200

    esphome lib update

--- components/blauberg_s14/fan/__init__.py
+++ components/blauberg_s14/fan/__init__.py
@@ -1,23 +1,23 @@
 import esphome.codegen as cg
 from esphome.components import fan, output
 import esphome.config_validation as cv
-from esphome.const import CONF_OUTPUT_ID
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["blauberg_s14"]
 
 blauberg_ns = cg.esphome_ns.namespace("blauberg_s14")
 BlaubergS14Fan = blauberg_ns.class_("BlaubergS14Fan", cg.Component, fan.Fan)
 
-CONFIG_SCHEMA = fan.FAN_SCHEMA.extend(
+CONFIG_SCHEMA = fan._FAN_SCHEMA.extend(
     {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(BlaubergS14Fan),
+        cv.GenerateID(CONF_ID): cv.declare_id(BlaubergS14Fan),
         cv.Required("blauberg_s14_id"): cv.use_id(blauberg_ns.BlaubergS14Controller),
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await fan.register_fan(var, config)
 
